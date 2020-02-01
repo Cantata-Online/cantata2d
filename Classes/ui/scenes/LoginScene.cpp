1396882d4401105411a3142fea0439dfadd31187
Sergei Beljakov <1899416+tasmanianfox@users.noreply.github.com>
2020-02-01 23:17:53 +0200

    Positioned elements on login screen, added the login button

--- Classes/ui/scenes/LoginScene.cpp
+++ Classes/ui/scenes/LoginScene.cpp
@@ -18,42 +18,54 @@ bool LoginScene::init()
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
 
-    auto label = Label::createWithTTF("Cantata Online", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        return false;
-    }
-    else
-    {
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-        this->addChild(label, 1);
-    }
+    Label* labelLogo = Label::createWithTTF("Cantata Online", "fonts/Marker Felt.ttf", 24);
+    labelLogo->setPosition(Vec2(origin.x + visibleSize.width/2,
+                                origin.y + visibleSize.height - labelLogo->getContentSize().height));
+    this->addChild(labelLogo, 1);
 
     Label* labelLogin = Label::createWithTTF("Login:", "fonts/arial.ttf", 12);
-    labelLogin->setPosition(Vec2(origin.x + visibleSize.width/4,
-                                 origin.y + visibleSize.height - labelLogin->getContentSize().height - 40));
+    labelLogin->setPosition(Vec2(origin.x + visibleSize.width/4 + labelLogin->getContentSize().width/2,
+                                 origin.y + 120));
     this->addChild(labelLogin);
 
 
     cocos2d::ui::TextField* textLogin = cocos2d::ui::TextField::create("john.doe", "Arial", 12);
-    textLogin->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - textLogin->getContentSize().height - 40));
+    textLogin->setPosition(Vec2(origin.x + visibleSize.width/2 + textLogin->getContentSize().width/2,
+                                origin.y + 120));
+    textLogin->setColor(Color3B::GRAY);
+    textLogin->setMaxLength(30);
+    textLogin->setMaxLengthEnabled(true);
     textLogin->setTextHorizontalAlignment(TextHAlignment::LEFT);
+    textLogin->setCursorEnabled(true);
+    textLogin->setTextAreaSize(Size(100, 15));
+    textLogin->setContentSize(Size(100, 15));
     this->addChild(textLogin);
 
     Label* labelPassword = Label::createWithTTF("Password:", "fonts/arial.ttf", 12);
-    labelPassword->setPosition(Vec2(origin.x + visibleSize.width/4,
-                                 origin.y + visibleSize.height - labelPassword->getContentSize().height - 60));
+    labelPassword->setPosition(Vec2(origin.x + visibleSize.width/4 + labelPassword->getContentSize().width/2,
+                                    origin.y + 100));
     this->addChild(labelPassword);
 
 
-    cocos2d::ui::TextField* textPassword = cocos2d::ui::TextField::create("john.doe", "Arial", 12);
-    textPassword->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - textPassword->getContentSize().height - 60));
-    textPassword->setTextHorizontalAlignment(TextHAlignment::LEFT);
+    cocos2d::ui::TextField* textPassword = cocos2d::ui::TextField::create("password", "Arial", 12);
+    textPassword->setPosition(Vec2(origin.x + visibleSize.width/2 + textPassword->getContentSize().width/2,
+                                   origin.y + 100));
+    textPassword->setCursorEnabled(true);
     textPassword->setPasswordEnabled(true);
+    textPassword->setColor(Color3B::GRAY);
+    textPassword->setMaxLength(30);
+    textPassword->setMaxLengthEnabled(true);
+    textPassword->setTextHorizontalAlignment(TextHAlignment::LEFT);
+    textPassword->setTextAreaSize(Size(100, 15));
+    textPassword->setContentSize(Size(100, 15));
     this->addChild(textPassword);
+
+    cocos2d::ui::Button *btnLogin = cocos2d::ui::Button::create();
+    btnLogin->setTitleText("Log In");
+    btnLogin->setPosition(Vec2(origin.x + visibleSize.width/2 + btnLogin->getContentSize().width/2,
+                                   origin.y + 80));
+
+    this->addChild(btnLogin);
 
 
     return true;
